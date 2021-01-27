b713e126ebd291590d9c9aec91b9d9bd40d808a8
dom <74149150+kiing-dom@users.noreply.github.com>
2021-01-27 18:21:24 +0000

    Update WeightConverter.java

--- WeightConverter.java
+++ WeightConverter.java
@@ -3,15 +3,17 @@ import java.util.*;
 class WeightConverter{
     public static void main(String[] args){
         double weight;
+        // add a scanner to input the value of a weight in KG
         Scanner in = new Scanner(System.in);
-
-        System.out.print("Enter weight in pounds ");
+        //adding a string to request the input of a value in the console
+        System.out.print("Enter weight in KG ");
+        //connecting the double "weight" to the scanner
         weight = in.nextInt();
-
-        weight = weight / 2.205;
-
-        System.out.println("Weight in Kilograms = " + weight);
-        
+        //assigning value for the weight using recursion
+        weight = weight * 2.205;
+        //print out the weight in pounds
+        System.out.println("Weight in LBS = " + weight);
+        //closed the scanner
         in.close();
         
     }
